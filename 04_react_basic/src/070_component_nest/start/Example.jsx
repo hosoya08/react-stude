a692b7c1865064d53b51f9a4bcd610f352157eab
hosoya08 <118666052+hosoya08@users.noreply.github.com>
2025-06-13 09:25:41 +0900

    [stude] componentの分割について

--- 04_react_basic/src/070_component_nest/start/Example.jsx
+++ 04_react_basic/src/070_component_nest/start/Example.jsx
@@ -1,11 +1,12 @@
-import "./Example.css";
+import Child from './components/Child'
 
-const Example = () => {
-  return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
-  );
-};
+// 読み込みが１つしかない場合、returnを省略できる
+const Example = () => <Child />
 
+
+```
+export名前付きとデフォルトの2種類あるが
+1ファイルに１つのコンポーネントを書くのが
+```
+一般的なのでdefaultを使用すれば良い
 export default Example;
