b219beba9c09f4d51aa78bf14d8191be5d2d2400
hosoya08 <118666052+hosoya08@users.noreply.github.com>
2026-03-10 08:51:32 +0900

    [stude]特別なプロパティ：childrenについて

--- 04_react_basic/src/110_props_children/start/Example.jsx
+++ 04_react_basic/src/110_props_children/start/Example.jsx
@@ -4,12 +4,29 @@ import Container from "./components/Container";
 const profile = [
   { name: "Takashi", age: 19, country: "Japan", color: "green" },
   { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Gabriel", age: 28, country: "Paraguay", color: "red" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      {/* 以下ではchildrenとしているが別に他の名目でも問題ない */}
+      <Container title="Childrenとは？"  children={
+        [
+          <Profile {...profile[0]}/> // コンポーネントの中に別のコンポーネントを渡すことができる
+        ]
+        }/>
+
+        {/*
+        Containerタグに終了タグを付けてその中に別のコンポーネントを入れることも可能。
+        でも、上記のchildrenをtestとして受け取るプロップスをchildrenからtestにすると以下のは表示されなくなる。
+        以下の値はchildrenでしか受け取ることが出来ないためである。
+        */}
+        <Container title="別の記述の方法（タグで囲う方法）">
+          <Profile {...profile[0]} />
+          <Profile {...profile[1]} />
+          <Profile {...profile[2]} />
+        </Container>
     </div>
   );
 };
