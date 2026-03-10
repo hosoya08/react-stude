30f510d17f9ca6dd185ef51f66b3856c46846086
hosoya08 <118666052+hosoya08@users.noreply.github.com>
2026-03-10 09:10:20 +0900

    [stude] propsの重要なルールについて

--- 04_react_basic/src/120_props_rules/start/Example.jsx
+++ 04_react_basic/src/120_props_rules/start/Example.jsx
@@ -1,14 +1,19 @@
-// POINT propsの流れは一方通行
-// POINT propsは読み取り専用
-
+// 対象動画：https://www.udemy.com/course/react-complete-guide/learn/lecture/33042084#questions
 import Bye from "./components/Bye"
 import Hello from "./components/Hello"
 
 const Example = () => {
+  // それぞれのコンポーネントに渡したい値がある場合親コンポーネントから渡すことしか出来ない
+  // Helloの中にname="taro"としてもそれはHelloの中でしか使えずByeにわたすことはできない
+
+  // POINT propsの流れは一方通行
+  const name = "Tom";
+
+
   return (
     <>
-      <Hello />
-      <Bye />
+      <Hello name={name}/>
+      <Bye name={name}/>
     </>
   );
 };
--- 04_react_basic/src/120_props_rules/start/components/Hello.jsx
+++ 04_react_basic/src/120_props_rules/start/components/Hello.jsx
@@ -1,4 +1,20 @@
 const Hello = (props) => {
+
+  // POINT propsは読み取り専用
+  // props.name = "Bob";
+  // このようにprops内容を変更しようとするとエラーになる
+
+  // propsの設定内容を確認する方法（変更可能かどうかなど）
+  const desc = Reflect.getOwnPropertyDescriptor(props, "name");// みたいオブジェクト、設定状態をみたいプロパティ
+  console.log(desc);
+
+  /*　出力結果
+  configurable : false 隠し設定の変更ができるかどうか　
+  enumerable : true forループで列挙になるかどうか
+  value : "Tom"
+  writable : false 対象のプロパティの値が変更できるかどうか
+  */
+
   return (
     <div>
       <h3>Hello {props.name}</h3>
