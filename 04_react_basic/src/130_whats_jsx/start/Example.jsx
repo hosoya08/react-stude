1ce2fb17ebfe035b98f6c1dae655f4d60ea297ee
hosoya08 <118666052+hosoya08@users.noreply.github.com>
2026-03-10 11:07:38 +0900

    [stude] jsxのながれについて（正体）

--- 04_react_basic/src/130_whats_jsx/start/Example.jsx
+++ 04_react_basic/src/130_whats_jsx/start/Example.jsx
@@ -1,11 +1,13 @@
+// https://www.udemy.com/course/react-complete-guide/learn/lecture/33042088#questions
 import React from "react";
 
 const Example = () => {
+    // 以下のサンプル1の中にあるhtmlはバベルというもので以下のコメントアウトしているモノに置き換えて出力している
   const sample1 = <h1 className="greeting">Hello World</h1>;
-
   // React.createElement("h1", {
   //   className: "greeting"
   // }, "Hello World");
+
 
   const sample2 = (
     <div>
@@ -14,6 +16,9 @@ const Example = () => {
     </div>
   );
 
+
+
+  // 以下はオブジェクトのpropsを出力している
   console.log(
     (
       <div>
@@ -22,13 +27,15 @@ const Example = () => {
       </div>
     ).props
   );
-
-  return React.createElement(
-    "div",
-    null,
+    // 上記の内容がバベルと同じ内容の処理が変更されて出力されている
+    // 以下は仮装ドムと言われる
+  return React.createElement("div",null,
     React.createElement("h1", null, "Hello!"),
     React.createElement("h2", null, "Good to see you.")
   );
 };
 
 export default Example;
+
+// jsxのながれPoint!!
+// jsxオブジェクト　→　バベルで関数変換　→　react要素
