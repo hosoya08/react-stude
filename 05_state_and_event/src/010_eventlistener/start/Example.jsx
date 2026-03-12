f770d51ccc3f62979288e6b8e5c6095b1b647711
hosoya08 <118666052+hosoya08@users.noreply.github.com>
2026-03-12 16:54:33 +0900

    [stide] イベントにあわせて関数を実行してみる

--- 05_state_and_event/src/010_eventlistener/start/Example.jsx
+++ 05_state_and_event/src/010_eventlistener/start/Example.jsx
@@ -1,6 +1,14 @@
 const Example = () => {
+
+  const clickHandler = () => {
+    alert('click on');
+  }
+
   return (
-    <></>
+    <>
+      <button onClick={clickHandler}>クリックしてください</button>
+      <button>クリックしてください</button>
+    </>
   );
 };
 
